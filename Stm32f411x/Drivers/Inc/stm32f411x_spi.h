fc2b72efb082ac3f4a9ca1e9d6ec546ed5be0eb6
YocoZuna <111345729+YocoZuna@users.noreply.github.com>
2022-10-24 22:49:25 +0200

    Adding SPI data send
    Adding SPI Data send

--- Stm32f411x/Drivers/Inc/stm32f411x_spi.h
+++ Stm32f411x/Drivers/Inc/stm32f411x_spi.h
@@ -39,6 +39,54 @@ typedef struct
 	SPIx_ConfigDef SPIConifg;
 }SPIx_Handle_t;
 
+/*
+ * Device mode
+ */
+#define SPI_DEVICE_MODE_MASTER	1
+#define SPI_DEVICE_MODE_SLAVE 	0
+
+/*
+ * Busconfig
+ */
+#define SPI_BUSCONFIG_FULL		0
+#define SPI_BUSCONFIG_HALF		1
+#define SPI_BUSCONFIG_TX_ONLY	2
+#define SPI_BUSCONFIG_RX_ONLY	3
+/*
+ * Clock speed of SPI
+ */
+#define SPI_SPEED_2				0
+#define SPI_SPEED_4				1
+#define SPI_SPEED_8				2
+#define SPI_SPEED_16			3
+#define SPI_SPEED_32			4
+#define SPI_SPEED_64			5
+#define SPI_SPEED_128			7
+#define SPI_SPEED_4257			7
+
+/*
+ *  Bit size
+ */
+#define SPI_DFF_8bit			0
+#define SPI_DFF_16bit			1
+
+/*
+ * CPOL
+ */
+#define SPI_CPOL_RISING			1
+#define SPI_CPOL_FALLING		0
+
+/*
+ * CPHA
+ */
+#define SPI_CPHA_First			0
+#define SPI_CPHA_Second			1
+
+/*
+ * NSS menagment
+ */
+#define SPI_NSS_SOFTWARE 		1
+#define SPI_NSS_HARDWARE		0
 
 #define SPI1 ((SPIx_RegDef*)DRV_SPI1)
 #define SPI2 ((SPIx_RegDef*)DRV_SPI2)
@@ -47,7 +95,7 @@ typedef struct
 
 
 void SPI_RCC(SPIx_RegDef *xSPI,uint8_t on_off);
-void SPI_Init(SPIx_RegDef *xSPI);
+void SPI_Init(SPIx_Handle_t *xSPI);
 void SPI_DeInit(SPIx_RegDef *xSPI);
 
 void SPI_Receive_Polling(SPIx_RegDef *xSPI, uint8_t *RXbuffor,uint32_t length);
--- Stm32f411x/Drivers/Src/stm32f411x_spi.c
+++ Stm32f411x/Drivers/Src/stm32f411x_spi.c
@@ -55,7 +55,48 @@ void SPI_RCC(SPIx_RegDef *xSPI,uint8_t on_off)
 		RCC_SPI4_DS();
 	}
 }
-void SPI_Init(SPIx_RegDef *xSPI);
+void SPI_Init(SPIx_Handle_t *xSPI)
+{
+	if(xSPI->SPIConifg.SPI_DeviceMode == SPI_DEVICE_MODE_MASTER)
+		/*
+		 * Device mode
+		 */
+		xSPI->pSPIx->SPI_CR1 |= (SPI_DEVICE_MODE_MASTER<<2);
+	else if (xSPI->SPIConifg.SPI_DeviceMode == SPI_DEVICE_MODE_SLAVE)
+		xSPI->pSPIx->SPI_CR1 |= (SPI_DEVICE_MODE_SLAVE<<2);
+	/*
+	 * Bus config
+	 * On restart configured as master
+	 */
+	if(xSPI->SPIConifg.SPI_BusConfig == SPI_BUSCONFIG_HALF)
+		xSPI->pSPIx->SPI_CR1 |= (xSPI->SPIConifg.SPI_BusConfig<<15);
+	else if (xSPI->SPIConifg.SPI_BusConfig == SPI_BUSCONFIG_TX_ONLY)
+		xSPI->pSPIx->SPI_CR1 |= (xSPI->SPIConifg.SPI_BusConfig<<15);
+	else if (xSPI->SPIConifg.SPI_BusConfig == SPI_BUSCONFIG_RX_ONLY)
+		xSPI->pSPIx->SPI_CR1 |= (xSPI->SPIConifg.SPI_BusConfig<<8);
+	/*
+	 * SSM
+	 */
+	if(xSPI->SPIConifg.SPI_SSM == SPI_NSS_SOFTWARE)
+		xSPI->pSPIx->SPI_CR1 |= (xSPI->SPIConifg.SPI_SSM<<9);
+	//TODO
+	/*
+	 * Clock speed
+	 */
+	xSPI->pSPIx->SPI_CR1 |= (xSPI->SPIConifg.SPI_CLKSpeed<<3);
+	/*
+	 * DFF
+	 */
+	xSPI->pSPIx->SPI_CR1 |= (xSPI->SPIConifg.SPI_DFF<<11);
+	/*
+	 * CPOL
+	 */
+	if(xSPI->SPIConifg.SPI_CPOL == SPI_CPOL_FALLING)
+		xSPI->pSPIx->SPI_CR1 |= (xSPI->SPIConifg.SPI_CPOL<<1);
+
+	if(xSPI->SPIConifg.SPI_CPHA == SPI_CPHA_Second)
+		xSPI->pSPIx->SPI_CR1 |= (xSPI->SPIConifg.SPI_CPHA<<1);
+}
 void SPI_DeInit(SPIx_RegDef *xSPI)
 {
 	if( SPI1 == xSPI)
@@ -77,7 +118,27 @@ void SPI_DeInit(SPIx_RegDef *xSPI)
 }
 
 void SPI_Receive_Polling(SPIx_RegDef *xSPI, uint8_t *RXbuffor,uint32_t length);
-void SPI_Send_Polling(SPIx_RegDef *xSPI, uint8_t *TXbuffor,uint32_t length);
+void SPI_Send_Polling(SPIx_RegDef *xSPI, uint8_t *TXbuffor,uint32_t length)
+{
+	while(len>0)
+	{
+		while(!(xSPI->SPI_SR &(1<<1))); // Wait for TX buffor empty
+
+		if(xSPI->SPI_CR1 & (1<11))
+		{
+			xSPI->SPI_DR = *((unt16_t*)TXbuffor);
+			length--;
+			length--;
+			(unt16_t*)TXbuffor++;
+		}
+		else
+		{
+			xSPI->SPI_DR = *TXbuffor;
+			length--;
+			TXbuffor++;
+		}
+		}
+}
 
 void SPI_IRQ_IT_Config(uint8_t IRQNumber,uint8_t on_off);
 void SPI_IRQHandler(SPIx_Handle_t *pHandle);

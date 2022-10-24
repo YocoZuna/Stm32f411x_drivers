87da678ed59e176cca524f786e025802ed773ae4
YocoZuna <111345729+YocoZuna@users.noreply.github.com>
2022-10-24 22:52:57 +0200

    Updating gitignore
    Updating gitignore

--- Stm32f411x/Debug/Drivers/Src/stm32f411x_gpio.d
+++ Stm32f411x/Debug/Drivers/Src/stm32f411x_gpio.d
@@ -1,7 +1,9 @@
 Drivers/Src/stm32f411x_gpio.o: ../Drivers/Src/stm32f411x_gpio.c \
- C:/Users/dawid/STM32CubeIDE/workspace_1.10.1/Stm32f411x_drivers/Stm32f411x/Drivers/Inc/stm32f411x_gpio.h \
- C:/Users/dawid/STM32CubeIDE/workspace_1.10.1/Stm32f411x_drivers/Stm32f411x/Drivers/Inc/stm32f411x.h \
- C:/Users/dawid/STM32CubeIDE/workspace_1.10.1/Stm32f411x_drivers/Stm32f411x/Drivers/Inc/stm32f411x_gpio.h
-C:/Users/dawid/STM32CubeIDE/workspace_1.10.1/Stm32f411x_drivers/Stm32f411x/Drivers/Inc/stm32f411x_gpio.h:
-C:/Users/dawid/STM32CubeIDE/workspace_1.10.1/Stm32f411x_drivers/Stm32f411x/Drivers/Inc/stm32f411x.h:
-C:/Users/dawid/STM32CubeIDE/workspace_1.10.1/Stm32f411x_drivers/Stm32f411x/Drivers/Inc/stm32f411x_gpio.h:
+ C:/Users/dawid/Documents/GitHub/Stm32f411x_drivers/Stm32f411x/Drivers/Inc/stm32f411x_gpio.h \
+ C:/Users/dawid/Documents/GitHub/Stm32f411x_drivers/Stm32f411x/Drivers/Inc/stm32f411x.h \
+ C:/Users/dawid/Documents/GitHub/Stm32f411x_drivers/Stm32f411x/Drivers/Inc/stm32f411x_gpio.h \
+ C:/Users/dawid/Documents/GitHub/Stm32f411x_drivers/Stm32f411x/Drivers/Inc/stm32f411x_spi.h
+C:/Users/dawid/Documents/GitHub/Stm32f411x_drivers/Stm32f411x/Drivers/Inc/stm32f411x_gpio.h:
+C:/Users/dawid/Documents/GitHub/Stm32f411x_drivers/Stm32f411x/Drivers/Inc/stm32f411x.h:
+C:/Users/dawid/Documents/GitHub/Stm32f411x_drivers/Stm32f411x/Drivers/Inc/stm32f411x_gpio.h:
+C:/Users/dawid/Documents/GitHub/Stm32f411x_drivers/Stm32f411x/Drivers/Inc/stm32f411x_spi.h:
--- Stm32f411x/Debug/Drivers/Src/subdir.mk
+++ Stm32f411x/Debug/Drivers/Src/subdir.mk
@@ -5,23 +5,26 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Drivers/Src/stm32f411x_gpio.c 
+../Drivers/Src/stm32f411x_gpio.c \
+../Drivers/Src/stm32f411x_spi.c 
 
 OBJS += \
-./Drivers/Src/stm32f411x_gpio.o 
+./Drivers/Src/stm32f411x_gpio.o \
+./Drivers/Src/stm32f411x_spi.o 
 
 C_DEPS += \
-./Drivers/Src/stm32f411x_gpio.d 
+./Drivers/Src/stm32f411x_gpio.d \
+./Drivers/Src/stm32f411x_spi.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 Drivers/Src/%.o Drivers/Src/%.su: ../Drivers/Src/%.c Drivers/Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DNUCLEO_F411RE -DSTM32 -DSTM32F4 -DSTM32F411RETx -c -I../Inc -I"C:/Users/dawid/STM32CubeIDE/workspace_1.10.1/Stm32f411x_drivers/Stm32f411x/Drivers/Inc" -I"C:/Users/dawid/STM32CubeIDE/workspace_1.10.1/Stm32f411x_drivers/Stm32f411x/Drivers/Src" -O0 -ffunction-sections -fdata-sections -Wall -fcommon -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DNUCLEO_F411RE -DSTM32 -DSTM32F4 -DSTM32F411RETx -c -I../Inc -I"C:/Users/dawid/Documents/GitHub/Stm32f411x_drivers/Stm32f411x/Drivers/Inc" -I"C:/Users/dawid/Documents/GitHub/Stm32f411x_drivers/Stm32f411x/Drivers/Src" -O0 -ffunction-sections -fdata-sections -Wall -fcommon -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Drivers-2f-Src
 
 clean-Drivers-2f-Src:
-	-$(RM) ./Drivers/Src/stm32f411x_gpio.d ./Drivers/Src/stm32f411x_gpio.o ./Drivers/Src/stm32f411x_gpio.su
+	-$(RM) ./Drivers/Src/stm32f411x_gpio.d ./Drivers/Src/stm32f411x_gpio.o ./Drivers/Src/stm32f411x_gpio.su ./Drivers/Src/stm32f411x_spi.d ./Drivers/Src/stm32f411x_spi.o ./Drivers/Src/stm32f411x_spi.su
 
 .PHONY: clean-Drivers-2f-Src
 
